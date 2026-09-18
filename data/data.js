b6c70ea1277688ac845fa35b9d2094481d393c4f
Georgin-codes <georginp.tecjob@gmail.com>
2026-09-18 11:31:33 -0500

    add projects page

---
+++ data/data.js
@@ -0,0 +1,10 @@
+export const data = [{
+
+    title:"CareerLens",
+    subtitle:"AI-Powered Resume & Job Description Analyzer",
+    description:"Career Lens is an AI-powered web application that analyzes a user's resume against a job description. Users upload their resume as a PDF and provide a job description, which the application processes through a Node.js and Express.js backend. The extracted resume content and job requirements are sent to an AI model for analysis, which identifies relevant skills, matching experience, potential gaps, and areas for improvement. The results are then streamed back to the user through the web interface in a clear, structured format.",
+    link:"https://youtu.be/znVqHRaDPdk"
+}]
+
+
+
---
+++ public/projects.js
@@ -0,0 +1,41 @@
+
+import { data } from "../data/data.js"
+const container = document.getElementById("container")
+
+
+
+function render(){
+    data.forEach((project)=>{
+    
+    const {title, subtitle, description}= project
+
+    container.innerHTML += `<div class="projects">
+
+                            <div class="heading">
+                                <h2 class="title">${title}</h2>
+                                <p class="subtitle">${subtitle}</p>
+                            </div>
+
+                            <div class="details">
+                                <p class="description">${description}</p>
+                                <div class="buttons">
+                                    <button class="btn" id="demo-btn" data-demo="1">Demo</button>
+                                    <button class="btn" id="github-btn" data-demo="2">GitHub</button>
+                                </div>
+                            </div>  
+
+                        </div>`
+
+    })
+
+}
+
+render()
+
+const demoBtn = document.getElementById("demo-btn")
+const GitHubBtn = document.getElementById("github-btn")
+
+demoBtn.addEventListener("click", ()=>{
+
+})
+
