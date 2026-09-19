fc0d1bf372a4cda89e65e76d30df2a081d5de08a
Georgin-codes <georginp.tecjob@gmail.com>
2026-09-19 12:16:14 -0500

    add side menu to projects page

--- public/projects.js
+++ public/projects.js
@@ -1,5 +1,7 @@
 
-// import { data } from "../data/data.js"
+const icon = document.getElementById('icon')
+const sideMenu = document.getElementById("side-menu")
+const closeBtn = document.getElementById("close-btn")
 const container = document.getElementById("container")
 
 
@@ -46,3 +48,10 @@ async function render(){
 render()
 
 
+icon.addEventListener("click",()=>{
+    sideMenu.classList.add("slide")
+})
+
+closeBtn.addEventListener("click", ()=>{
+    sideMenu.classList.remove("slide")
+})
