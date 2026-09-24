8cdaa92f79df184beb86defc62fc8de520a99a7c
Georgin-codes <georginp.tecjob@gmail.com>
2026-09-24 18:22:42 -0500

    improve contact form UI and API response handling

--- controller.js
+++ controller.js
@@ -28,6 +28,11 @@ export async function sendMail(req, res){
 
     console.log(req.body)
     const {name, email, subject, message} = req.body
+
+    if(!name || !email || !subject || !message){
+        return res.status(400).json({message:"Name, email, message are required"})
+    }
+
     try{
             const { data, error } = await resend.emails.send({
             from: 'My Portfolio <hello@georgeportfolio.online>',
@@ -44,7 +49,7 @@ export async function sendMail(req, res){
                 console.log(`resend error: ${error.message}`)
                 return res.status(400).json({message:"Something went wrong with sending email"})  
             }
-            return res.json({message:"Email has been sent"})
+            return res.json({message:"Email sent successfully"})
     }
     catch(error){
         res.status(500).json({message:`Error sending email, ${error.message}`})
--- public/contact.js
+++ public/contact.js
@@ -3,6 +3,10 @@ const sideMenu = document.getElementById("side-menu")
 const closeBtn = document.getElementById("close-btn")
 const Btn = document.getElementById('btn')
 const form = document.getElementById("form")
+const apiResponse = document.getElementById('api-response')
+const body =document.querySelector("body")
+const responseBody = document.getElementById("response")
+const responseClose = document.getElementById('response-close-btn')
 
 icon.addEventListener("click",()=>{
     sideMenu.classList.add("slide")
@@ -15,18 +19,29 @@ closeBtn.addEventListener("click", ()=>{
 Btn.addEventListener("click", async (e)=>{
 
     e.preventDefault()
+    body.style.backgroundColor = "#3A3A3A"
+    Btn.disabled = true
 
-    const name  = document.getElementById("name").value.trim().toLowerCase()
-    const email  = document.getElementById("email").value.trim().toLowerCase()
-    const subject  = document.getElementById("subject").value.trim().toLowerCase()
-    const message  = document.getElementById("message").value.trim().toLowerCase()
+    const nameInput = document.getElementById("name")
+    const emailInput = document.getElementById("email")
+    const subjectInput = document.getElementById("subject")
+    const messageInput = document.getElementById("message")
 
-    if(!name || !email || !subject || !message){
+    const name  = nameInput.value.trim().toLowerCase()
+    const email  = emailInput.value.trim().toLowerCase()
+    let subject  = subjectInput.value.trim().toLowerCase()
+    const message  = messageInput.value.trim().toLowerCase()
+
+    if(!name || !email || !message){
         return console.log("All fields are required")
     }
 
+    if(!subject){
+        subject = "Portfolio Contact Form Submission"
+    }
+
     try{
-        const response = await fetch("api/contact/submit", {
+        const res = await fetch("/api/contact/submit", {
         method:"POST",
         headers:{"Content-Type":"application/json"},
         body:JSON.stringify({
@@ -36,6 +51,19 @@ Btn.addEventListener("click", async (e)=>{
             message:message
             })
         })
+
+        if(res.ok){
+            nameInput.value = ""
+            emailInput.value = ""
+            subjectInput.value = ""
+            messageInput.value = ""
+        }
+
+        const data = await res.json()
+        console.log(data)
+        apiResponse.textContent = data.message
+        responseBody.style.display = "block"
+
     }
     catch(error){
         return console.log(`Unable to connect to the server. Please try again later. Error: ${error.message}`)
@@ -44,6 +72,10 @@ Btn.addEventListener("click", async (e)=>{
 })
     
 
-
+responseClose.addEventListener("click", ()=>{
+    responseBody.style.display = "none"
+    body.style.backgroundColor = "#252525"
+    Btn.disabled = false
+})
 
 
