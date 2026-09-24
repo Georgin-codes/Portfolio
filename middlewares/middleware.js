c1480a1d82f4d62538c9d4657b862f5d37bb9a78
Georgin-codes <georginp.tecjob@gmail.com>
2026-09-24 16:57:31 -0500

    add rate limit middleware

---
+++ middlewares/middleware.js
@@ -0,0 +1,13 @@
+import { rateLimit } from 'express-rate-limit'
+
+export const limiter = rateLimit({
+	windowMs: 24*60 * 60 * 1000, 
+	limit: 2, 
+	standardHeaders: 'draft-8', 
+	legacyHeaders: false, 
+	ipv6Subnet: 64, 
+    handler: (req, res)=>{
+        console.log("Rate limit reached")
+        res.status(429).json({message: "Too many form submission, try again later"})
+    }
+})
--- routes.js
+++ routes.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import { serveProjects, sendMail} from './controller.js'
+import { limiter } from './middlewares/middleware.js'
 
 export const renderRouter = express.Router()
 export const contactRouter = express.Router()
 
 
 renderRouter.get("/projects", serveProjects)
-contactRouter.post("/submit", sendMail)
+contactRouter.post("/submit", limiter, sendMail)
 
