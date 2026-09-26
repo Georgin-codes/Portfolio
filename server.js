e744fde628aee6d86012b4a30bd673215bc70902
Georgin-codes <georginp.tecjob@gmail.com>
2026-09-26 09:24:03 -0500

    fix rate limiter proxy configuration

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import { renderRouter, contactRouter } from './routes.js'
 const app = express()
 const PORT = process.env.PORT || 8000
 
+app.set("trust proxy", 1)
 app.use(express.json())
 app.use(express.static("public"))
 app.use('/api/contact', contactRouter)
